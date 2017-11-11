aef467cb41c0209ba7775304f2f71d2254e1b7e1
Maxim <maksim.astash@gmail.com>
2017-11-11 19:12:53 +0300

    Filling for DialogWindow.

--- PhysicsLabs/PhysicsLabs/MainWindow.xaml.cs
+++ PhysicsLabs/PhysicsLabs/MainWindow.xaml.cs
@@ -15,12 +15,65 @@ using System.Windows.Shapes;
 
 namespace PhysicsLabs
 {
-
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void GradeLb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (GradeCb.SelectedItem == null)
+            {
+                GradeLb.Visibility = Visibility.Collapsed;
+                GradeCb.IsDropDownOpen = true;
+
+            }
+        }
+
+        private void GradeCb_DropDownOpened(object sender, EventArgs e)
+        {
+            if (GradeCb.SelectedItem == null)
+            {
+                GradeLb.Visibility = Visibility.Collapsed;
+                GradeCb.IsDropDownOpen = true;
+
+            }
+        }
+
+        private void GradeCb_DropDownClosed(object sender, EventArgs e)
+        {
+            if (GradeCb.SelectedItem == null)
+                GradeLb.Visibility = Visibility.Visible;
+        }
+
+        private void LabLb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (GradeCb.SelectedItem == null)
+            {
+                LabLb.Visibility = Visibility.Collapsed;
+                LabCb.IsDropDownOpen = true;
+
+            }
+        }
+
+        private void LabeCb_DropDownOpened(object sender, EventArgs e)
+        {
+            if (GradeCb.SelectedItem == null)
+            {
+                GradeLb.Visibility = Visibility.Collapsed;
+                GradeCb.IsDropDownOpen = true;
+
+            }
+        }
+
+        private void LabCb_DropDownClosed(object sender, EventArgs e)
+        {
+            if (GradeCb.SelectedItem == null)
+                LabLb.Visibility = Visibility.Visible;
+        }
+
+        
     }
 }
