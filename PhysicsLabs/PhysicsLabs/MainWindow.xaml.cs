229c34d8ec160bbd12f972b59da717dcb4db5add
Maxim <maksim.astash@gmail.com>
2017-11-11 20:19:58 +0300

    The logo was drawn.The first dialog window was developed.

--- PhysicsLabs/PhysicsLabs/MainWindow.xaml.cs
+++ PhysicsLabs/PhysicsLabs/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PhysicsLabs.Labs;
 
 namespace PhysicsLabs
 {
@@ -50,7 +51,7 @@ namespace PhysicsLabs
 
         private void LabLb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (GradeCb.SelectedItem == null)
+            if (LabCb.SelectedItem == null)
             {
                 LabLb.Visibility = Visibility.Collapsed;
                 LabCb.IsDropDownOpen = true;
@@ -58,22 +59,45 @@ namespace PhysicsLabs
             }
         }
 
-        private void LabeCb_DropDownOpened(object sender, EventArgs e)
+        private void LabCb_DropDownOpened(object sender, EventArgs e)
         {
-            if (GradeCb.SelectedItem == null)
+            if (LabCb.SelectedItem == null)
             {
-                GradeLb.Visibility = Visibility.Collapsed;
-                GradeCb.IsDropDownOpen = true;
+                LabLb.Visibility = Visibility.Collapsed;
+                LabCb.IsDropDownOpen = true;
 
             }
         }
 
         private void LabCb_DropDownClosed(object sender, EventArgs e)
         {
-            if (GradeCb.SelectedItem == null)
+            if (LabCb.SelectedItem == null)
                 LabLb.Visibility = Visibility.Visible;
         }
 
-        
+        private void GradeCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LabCb.Items.Clear();
+            if (GradeCb.SelectedItem == GradeCb.Items[1])
+            {
+                LabCb.Items.Add("Изучение движения тел по окружности под действием силы упругости и тяжести.");
+                LabCb.Items.Add("Экспериментальное изучение закона сохранения механической энергии.");
+                LabCb.Items.Add("Опытная проверка закона Гей-Люссака.");
+                LabCb.Items.Add("Изучение законов последовательного и параллельного соединений проводников.");
+                LabCb.Items.Add("Измерение ЭДС и внутреннего сопротивления источника тока.");
+            }
+        }
+
+        private void LabCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LabCb.SelectedItem == LabCb.Items[0])
+            {
+                MessageBox.Show("Переходим");
+                TenOne tenOne = new TenOne();
+                this.Close();
+                tenOne.Show();
+            }
+                
+        }
     }
 }
